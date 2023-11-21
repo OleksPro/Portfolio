cb48a82302961677d7143ce41694afc23540eab8
Oleksandr Prokopenko <Oleksandr.Prokopenko92@gmail.com>
2023-11-21 23:34:06 +0200

    Adding html pages

--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render
 
 def home(request):
     return render(request, "main/home.html")
+
+def skills(request):
+    return render(request, "main/skills.html")
+
+def about(request):
+    return render(request, "main/about_me.html")
+
+def portfolio(request):
+    return render(request, "main/portfolio.html")
