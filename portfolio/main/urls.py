a76f6e3938145018b968d13a1126648992082d44
Oleksandr Prokopenko <Oleksandr.Prokopenko92@gmail.com>
2023-11-22 22:29:42 +0200

    Layout design

--- portfolio/main/urls.py
+++ portfolio/main/urls.py
@@ -3,8 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('skills', views.skills, name='skills'),
-    path('about', views.about, name='about'),
-    path('portfolio', views.portfolio, name='portfolio')
+    path('', views.home, name='home')
 ]
--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -2,12 +2,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "main/home.html")
-
-def skills(request):
-    return render(request, "main/skills.html")
-
-def about(request):
-    return render(request, "main/about_me.html")
-
-def portfolio(request):
-    return render(request, "main/portfolio.html")
