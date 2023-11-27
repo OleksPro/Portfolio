4ff7621abf04cb774aa4c079706792837688aac1
Oleksandr Prokopenko <Oleksandr.Prokopenko92@gmail.com>
2023-11-27 16:33:50 +0200

    Added my desctiption and style

--- portfolio/main/models.py
+++ portfolio/main/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    img_main = models.ImageField(upload_to='images', default='images/default.svg')
-    img_email = models.ImageField(upload_to='images', default='images/default.svg')
-    img_github = models.ImageField(upload_to='images', default='images/default.svg')
-    img_linkedin = models.ImageField(upload_to='images', default='images/default.svg')
-
+    img_main = models.ImageField(upload_to='images', blank=True)
+    img_email = models.ImageField(upload_to='images', blank=True)
+    img_github = models.ImageField(upload_to='images', blank=True)
+    img_linkedin = models.ImageField(upload_to='images', blank=True)
+    img_it = models.ImageField(upload_to='images', blank=True)
 
     def __str__(self):
         return self.title
