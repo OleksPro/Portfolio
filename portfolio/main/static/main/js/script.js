9e63eb7b2d89eb6dd60ed5819f855b207636a31c
OlexPro <oleks.prokopenko92@gmail.com>
2023-12-20 17:11:18 +0200

    Added contact form

--- portfolio/main/static/main/js/script.js
+++ portfolio/main/static/main/js/script.js
@@ -7,3 +7,16 @@ function filterSelection(category) {
   const selectedButton = document.querySelector(`.menu_link[data-category="${category}"]`);
   selectedButton.classList.add('active');
 }
+
+// Дозволяє вводити в поле тільки номер телефону
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('phoneInput').addEventListener('input', function (event) {
+      let inputValue = event.target.value;
+      let numericValue = inputValue.replace(/[^\d+]/g, ''); // Видалення всіх символів, крім цифр та "+"
+
+      // Розділити число на групи за 3 цифри, додаючи пробіл
+      let formattedValue = numericValue.replace(/(\d{3})(?=\d)/g, '$1 ');
+
+      event.target.value = formattedValue;
+  });
+});
