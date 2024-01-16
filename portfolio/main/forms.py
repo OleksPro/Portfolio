99da548b0cc6af877f6bfb5c229e34edd969d277
OlexPro <oleks.prokopenko92@gmail.com>
2024-01-16 15:20:23 +0200

    change placeholder in forms.py, add language links

--- portfolio/main/forms.py
+++ portfolio/main/forms.py
@@ -3,22 +3,22 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(
-        max_length=100, widget=forms.TextInput(
-        attrs={'placeholder': 'ENTER YOUR NAME*'}),
+        max_length=100, widget=forms.TextInput(),
         required=True
         )
-    email = forms.EmailField(widget=forms.TextInput(
-        attrs={'placeholder': 'ENTER YOUR EMAIL*'}), 
+    email = forms.EmailField(widget=forms.TextInput(), 
         required=True
         )
     phone_number = forms.CharField(
         max_length=13, 
-        widget=forms.TextInput(attrs={'placeholder': 'PHONE NUMBER'}),
-        required=False)
-    
+        widget=forms.TextInput(),
+        required=False
+        )
     message = forms.CharField(
-        widget=forms.Textarea(attrs={'placeholder': 'YOUR MESSAGE*'}), 
-        required=True)
+        max_length=1000,
+        widget=forms.Textarea(), 
+        required=True
+        )
 
     def __str__(self):
         return self.name
