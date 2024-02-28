4a517a6abe22cc1c481f0f9efb7c2add400f8dc2
OlexPro <oleks.prokopenko92@gmail.com>
2024-02-28 12:08:46 +0200

    change DB on MySQL

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -64,12 +64,17 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'test',
+            'USER': 'root',
+            'PASSWORD': '1992',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
     }
-}
 
 
 # Password validation
